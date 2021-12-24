d1bc50712eae6cfaee06e82659e44907fdfbbf6a
Edmund Dong <12579441+EdmundDong@users.noreply.github.com>
2021-12-24 00:02:16 -0700

    async data pull works

--- scripts/app.js
+++ scripts/app.js
@@ -2,59 +2,69 @@ var Airtable = require('airtable');
 var base = new Airtable({ apiKey: 'keyLfkfBG43VXN52y' }).base('app24peMma4LiqxIh');
 
 Vue.config.devtools = true
-columns = ["Student", "Overall Tier", "PVP Tier", "Raid Tier","ATK Type", "DEF Type", "Role", "Class", "DPS"]
+columns = ["Student", "Overall Tier", "PVP Tier", "Raid Tier", "ATK Type", "DEF Type", "Role", "Class", "DPS"]
 students = []
 
 async function pullData() {
-  await base('Tiers and Roster').select({
-    view: "Have",
-    fields: columns
-  }).eachPage(function page(records, fetchNextPage) {
-    records.forEach(function (record) {
-      console.log('Retrieved', record.fields.Student);
-      students.push(record.fields);
+  return new Promise((resolve, reject) => {
+    const students = []
+    base('Tiers and Roster').select({
+      view: "Have",
+      fields: columns
+    }).eachPage(function page(records, fetchNextPage) {
+      records.forEach(record => {
+        console.log('Retrieved', record.fields.Student)
+        students.push(record.fields)
+      });
+      fetchNextPage();
+    }, function done(err) {
+      if (err) {reject(err);} else {resolve(students);}
     });
-    fetchNextPage();
-  }, function done(err) {
-    if (err) { console.error(err); return; }
   });
-  return students
 }
 
-var allTable = new Vue({
-  el: '#allTable',
-  data: {
-    students: students,
-    headers: columns
-  }
-})
 heavy = []
-function heavyFormation() {
-  pullData()
+async function heavyFormation() {
+  students = await pullData()
   console.log('Students', students);
   let copy = students.slice();
   console.log('Heavy Formation', copy);
-  var result = students.filter(country => country.Student === 'Iori')
-  console.log('Heavy Formation', result);
+  const result = students.filter(student => student.Student == 'Iori')
+  console.log('Heavy Formation', result, students);
   heavy = result
   return result
 }
 
-heavyFormation()
-console.log('Heavy Formation', heavy);
-var heavyTable = new Vue({
-  el: '#heavyTable',
-  data: {
-    students: heavy,
-    headers: columns
-  }
-})
+async function main() {
+  heavy = await heavyFormation()
 
-console.log('Heavy Formation', heavy);
+  var allTable = new Vue({
+    el: '#allTable',
+    data: {
+      students: students,
+      headers: columns
+    }
+  })
 
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-})
+  console.log('Heavy Formation', heavy);
+  var heavyTable = new Vue({
+    el: '#heavyTable',
+    data: {
+      students: heavy,
+      headers: columns
+    }
+  })
+
+  console.log('Heavy Formation', heavy);
+
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+
+  
+}
+
+main()
